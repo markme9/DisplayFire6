7619e3d458e6a70940e2f6303218fedb09baf900
mark <mark.me017@gamil.com>
2020-06-23 15:43:48 +0530

    Initial Commit

---
+++ DisplayFire6/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  DisplayFire6
+//
+//  Created by mark me on 6/23/20.
+//  Copyright © 2020 mark me. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
